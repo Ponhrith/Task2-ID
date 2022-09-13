0837cba4e47b8b730a94275fc9a408552e7722a3
Ponhrith <ponhrith.nhep@gmail.com>
2022-09-13 16:24:17 +0700

    FEAT: Recursive Add User

--- ID/src/main/kotlin/Main.kt
+++ ID/src/main/kotlin/Main.kt
@@ -1,52 +1,12 @@
-import model.Person
-import java.io.BufferedWriter
+import helper.FileHelper
+import screen.UserScreen
 import java.io.File
-import java.io.FileWriter
 
 
 fun main(args: Array<String>) {
     val file = File("./ID.csv")
-    checkAndInitHeader(file)
+    FileHelper.checkAndInitHeader(file)
 
-    val person = Person()
-    println("Please enter your name: ")
-    person.name = readLine() ?: "Default Name"
-    println("Please enter your age: ")
-    person.age = readLine()?.toInt() ?: 0
-    println("Please enter your gender: ")
-    person.gender = readLine() ?: "Default Gender"
-    println("Please enter your height: ")
-    person.height = readLine()?.toFloat() ?: 0F
-    println("Please enter your address: ")
-    person.address = readLine() ?: "Default Address"
-    println("Please enter your contact: ")
-    person.contact = readLine() ?: "Default Contact"
-
-    val writtableData = person.toCSVData()
-    writeData(file, writtableData)
+    UserScreen().insertUserOperation(file)
+    println("===> Application has shut down")
 }
-
-private fun checkAndInitHeader(file: File) {
-    val firstLine = file.useLines { it.firstOrNull() }
-    val header = "Name,Age,Gender,Height,Address,Contact"
-    firstLine?.let {
-        if (it != header) { initHeader(file, header) }
-        else { println("PROC ==> Header already initialized for ID.csv") }
-    } ?: initHeader(file, header)
-}
-
-private fun initHeader(file: File, header: String){
-    println("PROC ==> Starting Initialized Header for ID.csv ...!")
-    writeData(file, header)
-    println("PROC ==> Done Initialized Header for ID.csv!")
-}
-
-private fun writeData(file: File, content: String) {
-    val fileWriter = FileWriter(file, true)
-    val bufferedWriter = BufferedWriter(fileWriter)
-    bufferedWriter.write(content)
-    bufferedWriter.close()
-}
-
-
-
